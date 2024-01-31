7cff04e01e456f475fb1219d8f72d3a52cf1b8b6
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2024-01-31 18:18:35 -0300

    front end folder structure

---
+++ src/app/home/events/[id].tsx/page.tsx
@@ -0,0 +1,4 @@
+export function EventPage() {
+  return <div>Eventpage</div>;
+}
+//aca creo que va a ser un redirect a /home/events/id/ticketsales
---
+++ src/app/home/events/[id].tsx/ticketsales/home.tsx
@@ -0,0 +1,3 @@
+export function TicketSales() {
+  return <div>TicketSales</div>;
+}
